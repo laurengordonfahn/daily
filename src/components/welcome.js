29b5e5c1a1d7b1049978bb2c5960ef50f78dd401
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-04-24 12:51:08 -0700

    Created Welcome Component
    Introduces users to the product before siging in

---
+++ src/components/welcome.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Welcome(props) {
+
+    return(
+        <div>
+            <h1> Welcome to Daily \n the emotion tracking calendar! </h1>
+            <h2> This calendar application helps you simply track your emotions each day. Just by typing in three adjectives and chosing an associated color! </h2> 
+
+            <h1> The Steps: </h1>
+            <h2> 1) Sign Up using a just your email </h2>
+            <h2> 2) Your personal private calendar will appear </h2>
+            <h2> 3) Start adding the day'&apos's three Adjectives and a Color for the day </h2>
+            
+            <h2>Chosing three adjectives that capture how you would summerize the feeling of the day and chose a color associated with the dominate mood of that day is all you need. These four pieces of information will enable this application to create an at a glance trend calendar of how your week, your month, your year is feeling. Explore your your mood trends with at a glance graphics as well!</h2>
+            
+            <h1> Why? This tool is used for so many reasons</h1>
+            <h2> 1) You are curious about yourself and your cool to follow </h2>
+            <h2> 2) You may have never been good at keeping a journal but most of us have to keep a calendar this makes it possible to do both at the same time! </h2>
+            <h2> 3) This helps us understand ourselves and you are worth it  </h2>  
+            <h2> 4) It can help you learn new words and think about you for 10 seconds a day on a deeper level </h2>    
+            <h2> It is a wonderful tool for those who need to track how they are doing to help doctors keep track of medications, we all need help sometimes keeping up withourselves! </h2>
+
+        </div>
+        )
+
+}
+
+export default Welcome;
