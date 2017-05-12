35de7110ab2146a93a8584cd120dc7a11cb5fb03
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-05-12 14:52:44 -0700

    Started test file for Server Enpoints

---
+++ server_tests.py
@@ -0,0 +1,22 @@
+import server
+import unittest
+
+class ServerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """ This supports try/catch test to produce status codes"""
+        self.client = server.app.test_client()
+        server.app.config['TESTING'] = True
+
+    def test_index(self):
+        client = server.app.test_client()
+
+        result = client.get('/')
+        self.assertIn('<div id="main"></div>', result.data)
+
+    def test_signUp(self):
+        client = server.app.test_client()
+
+        result = client.post('/signUp')
+        self.assertEqual(result.status_code, 200)
+
