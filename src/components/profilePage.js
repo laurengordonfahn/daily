34222ae6fa0dd9624e9bd2b66867aa9c4a030cfd
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-05-16 18:02:13 -0700

    Created profilePage
    Page to render emotion charts

---
+++ src/components/profilePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Profile from "./profile";
+import SignOut from "./signOut";
+import Chart from "./chart";
+
+
+class ProfilePage extends React.Component {
+    
+
+    render(){
+       
+        return(
+            <div> 
+                <Profile profile={this.props.profile} isLoggedIn={this.props.isLoggedIn} handleProfile={this.props.handleProfile} />
+                <SignOut onSignOut={this.props.onSignOut} />
+
+                <Chart />
+            </div>
+
+
+
+           
+            
+        );
+    }
+}
+
+export default ProfilePage;
