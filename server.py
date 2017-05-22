67d57212c4f866884526c70493149fa97263ac85
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-05-21 21:44:27 -0700

    Heroku commands in server.py

--- server.py
+++ server.py
@@ -307,13 +307,15 @@ def signOut():
 if __name__ == "__main__":
     import sys
 
-    app.debug = True
+    app.debug = True 
 
     connect_to_db(app)
 
     DebugToolbarExtension(app)
 
+    
+    DEBUG = "No_DEBUG" not in os.environ
     port='5000'
     if len(sys.argv) > 0:
         port = sys.argv[1]
-    app.run(host="0.0.0.0", port=int(port))
+    app.run(host="0.0.0.0", port=int(port), debug=DEBUG)
