a8a07a1ed3ea43418abc982215ca1c488994e2ae
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-05-22 14:54:20 -0700

    Added class for JWT
    Recourse: https://pythonhosted.org/Flask-JWT/

--- server.py
+++ server.py
@@ -36,6 +36,26 @@ ma = Marshmallow(app)
 
 app.secret_key = "pouring monday"
 
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = idself.username=username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+    def authenticate(username, password):
+        user=User.query.filter_by(username=username, password=password).first()
+
+       if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+            return user
+
+    def identity(payload):
+        user_id = payload['identity']
+        return User.query.filter_by(username=username, password=password).first().id
+
+jwt =JWT(app, authenticate, identity)
+
 ###################### class for Marshmellow #############################
 class UserSchema(ma.Schema):
     class Meta:
@@ -72,6 +92,10 @@ users_schema = UserSchema(many=True)
 #         }
 #     }
 #     return jsonify(response)
+@app.route('protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
 
 @app.route('/')
 def index():
@@ -314,6 +338,7 @@ if __name__ == "__main__":
 
     DebugToolbarExtension(app)
 
+    app.run()
 
     DEBUG = "No_DEBUG" not in os.environ
     port='5000'
